8a4bc32bb73893469732db2e8e94022b2b2ace4c
tombogle <tom_bogle@sil.org>
2024-03-13 16:36:15 -0500

    Added useExecutingAssembly flag to Analytics constructor to support plugins

--- src/SampleApp/Program.cs
+++ src/SampleApp/Program.cs
@@ -32,19 +32,19 @@ namespace SampleApp
 			userInfo.OtherProperties.Add("HowIUseIt","This is a really long explanation of how I use this product to see how much you would be able to extract from Mixpanel.\r\nAnd a second line of it.");
 
 			var propsForEveryEvent = new Dictionary<string, string> {{"channel", "beta"}};
-			using (new Analytics(args[0], userInfo, propertiesThatGoWithEveryEvent:propsForEveryEvent, clientType: clientType))
+			using (new Analytics(args[0], userInfo, propertiesThatGoWithEveryEvent: propsForEveryEvent, clientType: clientType))
 			{
 				Thread.Sleep(3000);
 				//note that anything we set from here on didn't make it into the initial "Launch" event. Things we want to 
 				//be in that event should go in the propertiesThatGoWithEveryEvent parameter of the constructor.
 
 				Analytics.SetApplicationProperty("TimeSinceLaunch", "3 seconds");
-				Analytics.Track("SomeEvent", new Dictionary<string, string>() {{"SomeValue", "62"}});
+				Analytics.Track("SomeEvent", new Dictionary<string, string> {{"SomeValue", "62"}});
 				Debug.WriteLine("Sleeping for 20 seconds to give it all a chance to send an event in the background...");
 				Thread.Sleep(20000);
 
 				Analytics.SetApplicationProperty("TimeSinceLaunch", "23 seconds");
-				Analytics.Track("SomeEvent", new Dictionary<string, string>() {{"SomeValue", "42"}});
+				Analytics.Track("SomeEvent", new Dictionary<string, string> {{"SomeValue", "42"}});
 				Console.WriteLine("Sleeping for another 20 seconds to give it all a chance to send an event in the background...");
 				Thread.Sleep(20000);
 
